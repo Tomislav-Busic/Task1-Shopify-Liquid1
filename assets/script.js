31015ba93fb9ca3111bbd031d0d68ad76b5e5b90
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-23 19:13:20 +0200

    max-width

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-
+const texts = [
+  " Make a statement with bold prints and vibrant colors that brighten up any outfit. ",
+  " Accessorize for that extra pop of style and to add your own personal touch to any ensemble. ",
+  " Step out of your comfort zone and try something new in fashion to create a unique look. ",
+  " Invest in timeless pieces that will never go out of style and can be worn season after season.",
+];
