846510099ed0ea2c7c41f8b1be0ef2afb90ba218
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-23 20:37:43 +0200

    responsive design

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,21 @@
+const container = document.querySelector(".text-container");
+const paragraph = document.querySelector("p");
+const btn = document.querySelector("button");
+let index = 0;
+
 const texts = [
   " Make a statement with bold prints and vibrant colors that brighten up any outfit. ",
   " Accessorize for that extra pop of style and to add your own personal touch to any ensemble. ",
   " Step out of your comfort zone and try something new in fashion to create a unique look. ",
   " Invest in timeless pieces that will never go out of style and can be worn season after season.",
 ];
+
+btn.addEventListener("click", () => {
+  paragraph.innerHTML = texts[index];
+  index = index === texts.length - 1 ? 0 : index + 1;
+  paragraph.classList.add("animation");
+});
+
+container.addEventListener("animationend", () => {
+  paragraph.classList.remove("animation");
+});
